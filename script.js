9a240262fce976e3382dd3b7ec7c8d436a158aa9
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-03 22:08:29 +0900

    completed

--- script.js
+++ script.js
@@ -8,19 +8,24 @@ const item = {
     }
 }
 
-document.getElementById("name").textContent = "商品名は" + item["name"];
-document.getElementById("price").textContent = "価格は" + item["price"] + "円です。";
+item["c"] = "テスト2";
+
+document.getElementById("name").textContent = "商品名は" + item.name;
+document.getElementById("price").textContent = "値段は" + item["price"] + "円です。";
 if (item["sale"]){
-    document.getElementById("sale").textContent = item["name"] + "セール中です。";
+    document.getElementById("sale").textContent = item["name"] + "はセール中です。"
 }
 
 let texts = '';
-for(let i = 0;i < item["parts"].length; i++){
+for (let i = 0; i < item["parts"].length; i++){
     texts += item["parts"][i];
-    if (i !== item["parts"].length -1){
-        texts += ".";
+    if(texts !== item["parts"].length-1){
+        texts += "・";
     }
 }
+document.getElementById("parts").textContent = item["name"] + "のパーツは" + texts + "です。";
 
-document.getElementById("parts").textContent = item["name"] + "のパーツは" + texts + "です。"
 document.getElementById("test").textContent = item["a"]["b"];
+
+console.log(item);
+console.table(item);
